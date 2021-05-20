58beb99cbdf95e434e69137f23ad25f1843f6e2b
ABtheAwesome <79143202+ABtheAwesome@users.noreply.github.com>
2021-05-20 10:24:30 +0200

    added python notebook (detect_body_pose)

--- detect_body_pose.py
+++ detect_body_pose.py
@@ -80,13 +80,12 @@ def start_recognizing_body_pose(model_class, model_config, model_weights, datase
     dataset = dset.ImageFolder(dataset_base_path, transform=transform)
     test_loader = DataLoader(dataset, batch_size=1,
                               shuffle=True, num_workers=0)
-    img, label = test_loader
 
     print(f"The type of the loaded dataset is: {type(test_loader)}")
     print(f"Shape of loaded dataset is: {np.shape(test_loader)}")
-    print(f"The type of the img is: {type(img)}")
-    print(f"Shape of img is: {np.shape(img)}")
-    plt.imshow(img, cmap='gray')
+    #print(f"The type of the img is: {type(img)}")
+    #print(f"Shape of img is: {np.shape(img)}")
+    plt.imshow(test_loader, cmap='gray')
 
 
 
